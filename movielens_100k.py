d30fd92c83b7ea5f1d5ced7b7669e39d376096af
kyrie0109 <wangfuyi15@mails.tsinghua.edu.cn>
2018-03-18 11:10:33 +0800

    Update movielens_100k.py

--- movielens_100k.py
+++ movielens_100k.py
@@ -19,9 +19,19 @@ from compute_grad import compute_grad
 ratings_file = 'F:/Adversarial machine learning/movielens-master/code/ratings-ml.csv'
 ratings = load_movielens_ratings(ratings_file)
 rand_state = RandomState(0)
-#667 * 9799
+
 max_rating = max(ratings[:, 2])
 min_rating = min(ratings[:, 2])
+'''
+parameters:
+lamda_u: the regularization parameter of user
+lamda_v: the regularization parameter of item
+alpha: the proportion of malicious users
+B: the items of malicious users rating
+n_iter: number of iteration
+converge: the least RMSE between two iterations
+train_pct: the proportion of train dataset
+'''
 lamda_u = 5e-2
 lamda_v = 5e-2
 alpha = 0.2
@@ -53,16 +63,15 @@ for u in xrange(mal_user):
         mal_rating = 2 * (RandomState(seed).rand() > 0.5) - 1
         mal_ratings.append([mal_user_idx, mal_movie_idx, mal_rating])
 mal_ratings = np.array(mal_ratings)
-
+#initialize the matrix U U~ and V 
 user_features_ = 0.1 * RandomState(seed).rand(n_user, n_feature)
 mal_user_features_ = 0.1 * RandomState(seed).rand(mal_user, n_feature)
 item_features_ = 0.1 * RandomState(seed).rand(n_item, n_feature)
-#print(mal_ratings_csr_[3, :].nonzero())
 mean_rating_ = np.mean(train.take(2, axis=1))
 mal_mean_rating_ = np.mean(mal_ratings.take(2, axis=1))
 user_features_origin_ = 0.1 * RandomState(seed).rand(n_user, n_feature)
 item_features_origin_ = 0.1 * RandomState(seed).rand(n_item, n_feature)
-'''
+
 #train origin model
 for iteration in xrange(n_iters):
     t1 = time.time()
@@ -91,11 +100,17 @@ for iteration in xrange(n_iters):
         break
     else:
         last_rmse = train_rmse
-'''
 
+#using the algorithm of PGA to optimize the utility function
+'''
+m_iters: number of iteration in PGA
+s_t: step size 
+Lamda: 
+'''
 m_iters = 10
 s_t = 0.2 * np.ones([m_iters])
 converge = 1e-5
+Lamda = 1
 last_rmse = None
 for t in xrange(m_iters):
     t1 = time.time()
@@ -103,6 +118,8 @@ for t in xrange(m_iters):
                         item_features_, lamda_v, n_feature, user_features_origin_, item_features_origin_)
     temp = mal_user_features_
     mal_user_features_ +=  grad_total * s_t[t]
+    mal_user_features_[mal_user_features_ > Lamda] = Lamda
+    mal_user_features_[mal_user_features_ < - Lamda] = - Lamda
     rmse = rmse(mal_user_features_, temp)
     t2 = time.time()
     print("The %d th iteration \t time: %ds \t RMSE: %f " % (t + 1, t2 - t1, rmse))
@@ -112,5 +129,3 @@ for t in xrange(m_iters):
         last_rmse = rmse
     
 
-#for iter in  attack_iters:
-#attack_preds = compuer_utility_gradient(ratings.take([0, 1], axis=1)
